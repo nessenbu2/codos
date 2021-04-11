e8db142f63409099c50cbbf3d6e0dcf8a2b0f0a6
Nick Essenburg <nessenbu@gmail.com>
2021-04-11 13:50:12 -0700

    Use generated board to display on client side.
    Next is having the client actually able to make changes to the server
    and have that reflect on refresh/other browsers without a refresh

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  state = {
-    data: null
-  };
+class Score extends Component {
+  render() {
+    return (
+      <div className="score-board">
+        <h1 className="score-red">8</h1>
+        <h1> - </h1>
+        <h1 className="score-blue">9</h1>
+      </div>
+    );
+  }
+}
+
+class Tile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value,
+      color: props.tileColor,
+      selected: false
+    };
+  }
+  render() {
+    return (
+      <button
+        className={"tile " + (this.state.selected ? "selected-" + this.state.color : "not-selected")}
+        onClick={() => this.setState({selected: true})}
+      >
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+class Board extends Component {
 
   componentDidMount() {
     // Call our fetch function below once the component mounts
     this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
+      .then(res => this.setState({ data: JSON.parse(res.codies)}))
       .catch(err => console.log(err));
   }
+
   // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
   callBackendAPI = async () => {
     const response = await fetch('/express_backend');
@@ -25,17 +55,76 @@ class App extends Component {
   };
 
   render() {
+    var display = []
+    if (this.state !== null) {
+      var index = 0;
+      var row = [];
+      for (const tile of this.state.data.tiles) {
+        row.push(<Tile value={tile.word} tileColor={tile.color}/>)
+        index++;
+        if (index % 5 === 0) {
+          display.push(<div className="board-row">{row}</div>);
+          row = [];
+        }
+      }
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        // Render the newly fetched data inside of this.state.data 
-        <p className="App-intro">{this.state.data}</p>
+      <div className="game-area">
+        <div className="board">
+          {display}
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Codos">
+        <Score />
+        <Board />
       </div>
     );
   }
 }
 
 export default App;
+
+        //<div className="board">
+        //  <div className="board-row">
+        //    <Tile value={"ABCD"} tileColor={"gray"}/>
+        //    <Tile value={"ABCD"} tileColor={"gray"}/>
+        //    <Tile value={"ABCD"} tileColor={"red"}/>
+        //    <Tile value={"ABCD"} tileColor={"black"}/>
+        //    <Tile value={"ABCD"} tileColor={"blue"}/>
+        //  </div>
+        //  <div className="board-row">
+        //    <Tile value={"ABCD"} tileColor={"blue"}/>
+        //    <Tile value={"ABCD"} tileColor={"gray"}/>
+        //    <Tile value={"ABCD"} tileColor={"red"}/>
+        //    <Tile value={"ABCD"} tileColor={"black"}/>
+        //    <Tile value={"ABCD"} tileColor={"blue"}/>
+        //  </div>
+        //  <div className="board-row">
+        //    <Tile value={"ABCD"} tileColor={"blue"}/>
+        //    <Tile value={"ABCD"} tileColor={"gray"}/>
+        //    <Tile value={"ABCD"} tileColor={"red"}/>
+        //    <Tile value={"ABCD"} tileColor={"black"}/>
+        //    <Tile value={"ABCD"} tileColor={"blue"}/>
+        //  </div>
+        //  <div className="board-row">
+        //    <Tile value={"ABCD"} tileColor={"blue"}/>
+        //    <Tile value={"ABCD"} tileColor={"gray"}/>
+        //    <Tile value={"ABCD"} tileColor={"red"}/>
+        //    <Tile value={"ABCD"} tileColor={"black"}/>
+        //    <Tile value={"ABCD"} tileColor={"blue"}/>
+        //  </div>
+        //  <div className="board-row">
+        //    <Tile value={"ABCD"} tileColor={"blue"}/>
+        //    <Tile value={"ABCD"} tileColor={"gray"}/>
+        //    <Tile value={"ABCD"} tileColor={"red"}/>
+        //    <Tile value={"ABCD"} tileColor={"black"}/>
+        //    <Tile value={"ABCD"} tileColor={"blue"}/>
+        //  </div>
+        //</div>
--- server/server.js
+++ server/server.js
@@ -9,12 +9,15 @@ const port = process.env.PORT || 5000;
 const fs = require('fs');
 const parse = require('csv-parse');
 
+var codies = new Codies();
+
+console.log(JSON.stringify(codies));
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+  res.send({ codies: JSON.stringify(codies)});
 });
 
-var codies = new Codies();
